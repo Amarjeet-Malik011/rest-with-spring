8973aa1eca137c2386dda8ab89e9a3dc36fd9297
Amarjeet-Malik011 <ch.amarjeetmalik@gmail.com>
2019-03-29 14:19:55 +0530

    commiting rest with spring assignment- final correction

--- src/main/java/com/spring/rest/springrest/entities/User.java
+++ src/main/java/com/spring/rest/springrest/entities/User.java
@@ -1,20 +1,36 @@
 package com.spring.rest.springrest.entities;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 
+//@JsonFilter("myFilter")
+//@JsonIgnoreProperties(value = "id")
 @Entity
 public class User {
 
+//    @JsonIgnore
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Size(min=2,message = "name should have at least 2 characters")
     private String name;
 
     private Integer age;
+
+    public User(@Size(min = 2, message = "name should have at least 2 characters") String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
 
     public Long getId() {
         return id;
--- src/main/java/com/spring/rest/springrest/services/UserService.java
+++ src/main/java/com/spring/rest/springrest/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +16,18 @@ public class UserService {
 
     public List<User> getAllUsers(){
         return (List<User>)userRepository.findAll();
+    }
+
+    public User getUserById(Long id){
+        Optional<User>  optional=userRepository.findById(id);
+        return optional.isPresent() ? optional.get() :null;
+    }
+
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+    public void deleteUser(User user){
+        userRepository.delete(user);
     }
 }
